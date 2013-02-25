207773c9ffa2a41b554555081b3fda562953fb81
Piwosz <mojepiwo@gmail.com>
2013-02-25 22:31:11 +0100

    More testing input in example.asm

--- bin/example.asm
+++ bin/example.asm
@@ -2,7 +2,24 @@
 db MojePiwoHelloWorld "Hello World; Teapot 0.1"
 ; Begin of the code segment
 .code
-mov ax, @MojePiwoHelloWorld; Pass address of MojePiwoHelloWorld to ax
-push ax
-call TEAPrintFunction
+	mov ax, @MojePiwoHelloWorld; Pass address of MojePiwoHelloWorld to ax
+	push ax
+	call TEAPrintFunction
+	pop ax ; Get result from stack
+	mov bx, 0
+	cmp ax, bx
+	jne failedToPrint
+
+.failedToPrint
+	push ax
+	mov ax, 0x0h ; exit function
+	int 0x01h ; call to kernel
+
+.float;Instruction for FPU
+	mov fax, 0.1
+.numberTest
+	mov ecx, 1000
+
+.hexTest
+	jmp 0x10h
 
