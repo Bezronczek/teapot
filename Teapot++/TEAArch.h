917a2ce0e2cc3bd61ae1f39e55c8b73609ebe42a
Piwosz <mojepiwo@gmail.com>
2013-02-27 20:44:30 +0100

    Refactored code. It was wrong way

---
+++ Teapot++/TEAArch.h
@@ -0,0 +1,101 @@
+/*
+ * TEAArch.h
+ *
+ *  Created on: 26-02-2013
+ *      Author: Rafal Czaja
+ */
+
+#ifndef TEAARCH_H_
+#define TEAARCH_H_
+#include <map>
+#include <string>
+#include <fstream>
+#include <iostream>
+//class TEASerialize {
+//public:
+//	TEASerialize() {
+//	}
+//
+//	virtual int serialize() = 0;
+//};
+//
+//class TEARegister: public TEASerialize {
+//public:
+//	enum RegSize {
+//		B64 = 0, B32 = 1, B16 = 2, B8 = 3
+//	};
+//	TEARegister(unsigned int id, int size) :
+//			m_iId(id), m_iRegSize(size) {
+//	}
+//	virtual ~TEARegister() {
+//	}
+//	int serialize() {
+//
+//		return 0;
+//	}
+//private:
+//	unsigned int m_iId;
+//	int m_iRegSize;
+//};
+//template<class T>
+//class TEAHelper {
+//public:
+//	TEAHelper(std::map<std::string, TEASerialize*> *pMap) :
+//			m_pMap(pMap) {
+//	}
+//	bool generateMap(int arg, Json::Value &json) {
+//		bool bRet = true;
+//		for (int x = 0; x < json.size(); x++) {
+//			std::string name = (json[x]).getMemberNames()[0];
+//			unsigned int id = (json[x]).get(name, -1).asUInt();
+//			m_pMap->insert(std::pair<std::string, TEASerialize*>(name, new T(id, arg)));
+//		}
+//		return bRet;
+//	}
+//private:
+//	std::map<std::string, TEASerialize*> *m_pMap;
+//};
+
+class TByte {
+public:
+	TByte() {
+	}
+	virtual unsigned int getRepresetation() = 0;
+};
+
+class TRegister: public TByte {
+public:
+	TRegister(unsigned int iId) :
+			m_iId(iId) {
+	}
+	virtual ~TRegister() {
+	}
+	unsigned int getRepresatation() {
+		return m_iId;
+	}
+private:
+	unsigned int m_iId;
+};
+class TInstruction: public TByte {
+public:
+	TInstruction(unsigned int iId) :
+			m_iId(iId) {
+	}
+	virtual ~TInstruction() {
+	}
+	unsigned int getRepresatation() {
+		return m_iId;
+	}
+private:
+	unsigned int m_iId;
+};
+
+class TEAArch {
+public:
+	TEAArch(){}
+	virtual ~TEAArch(){}
+	virtual TInstruction *getInstruction(const std::string& symbol) = 0;
+
+};
+
+#endif /* TEAARCH_H_ */
--- makefile
+++ makefile
@@ -2,8 +2,8 @@
 #	Author: Rafał Czaja
 #	Date: 25-02-13
 
-all: Teapot++/main.cpp Teapot++/parser.h Teapot++/parser.cpp Teapot++/TEALex.cpp
-	g++ $^ -o ./bin/tc --std=c++0x  -lboost_program_options
+all: Teapot++/main.cpp Teapot++/parser.h Teapot++/parser.cpp Teapot++/TEALex.cpp Teapot++/TEAParser.cpp
+	g++ $^ -o ./bin/tc --std=c++0x  -lboost_program_options -ljson -I./lib/jsoncpp-src-0.5.0/include -L./lib/jsoncpp-src-0.5.0/libs
 
 clean:
 	rm -rf *.o 
