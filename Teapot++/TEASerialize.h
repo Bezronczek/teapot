96cf36ad3fa137f1159083edf41e11b21bfd1ae8
Piwosz <mojepiwo@gmail.com>
2013-03-02 16:22:52 +0100

    New class for setting properly bitmasks

--- Teapot++/TEASerialize.h
+++ Teapot++/TEASerialize.h
@@ -16,6 +16,54 @@ public:
 	virtual unsigned int get() const = 0;
 };
 
+class TEABitmask {
+public:
+	static unsigned int getInstructionId(unsigned int bitmask) {
+		return (bitmask & m_uiRightBitmask);
+	}
+	static unsigned int getInstructionParams(unsigned int bitmask) {
+		return ((bitmask & m_uiLeftBitmask) >> 8);
+	}
+
+	static void setInstructionCout(unsigned int &bitmask, int count) {
+		bitmask = ((count << 9) & m_uiArgsCountMask) | bitmask;
+	}
+
+	static void setInstructionMagicBit(unsigned int &bitmask) {
+		bitmask = bitmask | m_uiInstructionMagicBit;
+	}
+
+	static void setFirstArgAsRegister(unsigned int &bitmask) {
+		bitmask = bitmask | m_uiFirstArgumentMask;
+	}
+
+	static void setFirstArgAsSpecial(unsigned int &bitmask) {
+		bitmask = bitmask | m_uiFirstArgumentIsSpecial;
+	}
+
+	static void setSecondArgAsRegister(unsigned int &bitmask) {
+		bitmask = bitmask | m_uiSecondArgumentMask;
+	}
+
+	static void setSecondArgAsSpecial(unsigned int &bitmask) {
+		bitmask = bitmask | m_uiSecondArgumentIsSpecial;
+	}
+
+private:
+	static const unsigned int m_uiLeftBitmask = 0xff00;
+	static const unsigned int m_uiRightBitmask = 0x00ff;
+
+	static const unsigned int m_uiInstructionMagicBit = 0x8000;
+
+	static const unsigned int m_uiArgsCountMask = 0x600;
+	static const unsigned int m_uiFirstArgumentMask = 0x4000;
+	static const unsigned int m_uiFirstArgumentIsSpecial = 0x2000;
+	static const unsigned int m_uiSecondArgumentMask = 0x1000;
+	static const unsigned int m_uiSecondArgumentIsSpecial = 0x800;
+
+	static const unsigned int m_uiReservedBit = 0x100;
+};
+
 class TNode {
 public:
 	enum ArgType {
